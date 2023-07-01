5b7b4ff46df43e6cbf09463838fa4df3f564ce1e
neketli <neketli.cspl@gmail.com>
2023-07-01 13:49:00 +0300

    Added some styles and basic settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	const endpoint = 'https://jsonplaceholder.typicode.com/posts';
+	let posts: { title: string }[] = [{ title: '123' }];
+
+	onMount(async function () {});
+</script>
+
+<section class="hero">
+	<h1>Здравствуйте!</h1>
+	<h2>Приглашаем вас на свадьбу!</h2>
+	<p>Всем привет</p>
+	<img src="/src/assets/1.jpg" alt="это мы" srcset="" />
+</section>
+
+<style lang="scss">
+	.hero {
+		padding: 20px 30px;
+		margin: 0 auto;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		h1 {
+			font-size: 50px;
+		}
+	}
+</style>
